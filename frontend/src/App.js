60ea187e25ec7653a9e861d25ff8375c7c5ca4a5
Yash Marmat <yashmarmat08@gmail.com>
2021-07-05 23:58:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import NavBar from './components/Navbar';
 import ArticleDetailsPage from './pages/ArticleDetailsPage';
 import WelcomePage from './pages/WelcomePage';
